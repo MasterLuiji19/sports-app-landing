f6669cb4c06d24c75d26753c5e90137d9fdcf659
MasterLuiji <master199037@gmail.com>
2019-04-20 17:08:40 +0300

    update index, main, add js for home

---
+++ src/scroll.js
@@ -0,0 +1,26 @@
+$('.twitter').slick({
+  dots: true,
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
